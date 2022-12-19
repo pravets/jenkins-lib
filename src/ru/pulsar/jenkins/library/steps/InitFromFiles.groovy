6ae8cf9b4fa3b1b45d1457eb8bd650f72bdd50eb
Iosif Pravets <chamaileontas@ya.ru>
2022-12-19 16:44:37 +0300

    Правка ошибок

--- src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
+++ src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
@@ -45,10 +45,10 @@ class InitFromFiles implements Serializable {
             extNames = config.extNames
 
             if (srcExtDir.size() > 0 && extNames.size() > 0) {
-                srcExtBuildDir = "$env.WORKSPACE/$EdtToDesignerFormatTransformation.EXT_DIR"
+                srcExtBuildDir = "$env.WORKSPACE/$EdtToDesignerFormatTransformation.EXT_DIR/${it}"
                 extNames.each {
                     steps.unstash("ext-${it}-zip")
-                    steps.unzip("$srcExtBuildDir/${it}", "build/ext-${it}.zip")
+                    steps.unzip("$srcExtBuildDir", "build/ext-${it}.zip")
                 }
             }
 
@@ -64,7 +64,7 @@ class InitFromFiles implements Serializable {
         if (srcExtDir.size() > 0 && extNames.size() > 0) {
                 extNames.each {
                     Logger.println("Выполнение загрузки конфигурации из файлов")
-                    def initExtCommand = "$vrunnerPath compileext $srcExtBuildDir\${it} ${it} --ibconnection \"/F./build/ib\" --updatedb"
+                    def initExtCommand = "$vrunnerPath compileext $srcExtBuildDir ${it} --ibconnection \"/F./build/ib\" --updatedb"
                     VRunner.exec(initExtCommand)
                 }
         }
