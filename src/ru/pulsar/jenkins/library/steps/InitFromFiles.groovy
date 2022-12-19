4697807edb4f51f5de2ed96a8ff5ffae4c8c1d00
Iosif Pravets <chamaileontas@ya.ru>
2022-12-19 13:20:29 +0300

    правка ошибок

--- src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
+++ src/ru/pulsar/jenkins/library/steps/InitFromFiles.groovy
@@ -45,7 +45,6 @@ class InitFromFiles implements Serializable {
             extNames = config.extNames
 
             if (srcExtDir.size() > 0 && extNames.size() > 0) {
-                Logger.println("extNames $extNames")
                 srcExtBuildDir = "$env.WORKSPACE/$EdtToDesignerFormatTransformation.EXT_DIR"
                 extNames.each {
                     Logger.println("unstash ext-${it}-zip")
